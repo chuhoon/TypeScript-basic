71dd9eac32a2156318445001737fdc71d0b3d0af
chuhoon <khchu1220@naver.com>
2023-04-01 11:48:46 +0900

    Study : 기본 타입 문자열 숫자 배열

---
+++ study/typs-basic.ts
@@ -0,0 +1,10 @@
+// js 선언 방식
+const str = 'hello';
+const arr = [1, 2, 3];
+
+// ts 선언 방식
+const strType: string = 'hello';
+const num: number = 10;
+const arrType: Array<number> = [1, 2, 3]; // 배열 속 숫자만
+const arrStrType: Array<string> = ['a', 'b', 'c']; // 배열 속 문자
+const arrNumberType: number[] = [1, 2, 3];
