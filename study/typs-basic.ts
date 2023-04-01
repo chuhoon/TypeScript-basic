b92ade3b5321afed77bd4d3787f7f97edefbd57a
chuhoon <khchu1220@naver.com>
2023-04-01 11:53:27 +0900

    Study : 기본 타입 튜플 객체 진위값

--- study/typs-basic.ts
+++ study/typs-basic.ts
@@ -2,9 +2,28 @@
 const str = 'hello';
 const arr = [1, 2, 3];
 
-// ts 선언 방식
+// TS 선언 방식
 const strType: string = 'hello';
 const num: number = 10;
 const arrType: Array<number> = [1, 2, 3]; // 배열 속 숫자만
 const arrStrType: Array<string> = ['a', 'b', 'c']; // 배열 속 문자
 const arrNumberType: number[] = [1, 2, 3];
+
+// TS 튜플 : 배열의 index의 타입이 정해져있다.
+const address: [string, number, string] = ['강남', 1, '평촌'];
+
+// TS 객체
+const obj: object = {};
+const person: object = {
+  name: 'chu',
+  age: 28,
+};
+
+// object 구체적인 타입 설정
+const personType: { name: string; age: number } = {
+  name: 'chu',
+  age: 28,
+};
+
+// TS 진위값
+const show: boolean = true;
