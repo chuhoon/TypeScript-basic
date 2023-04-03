3d160727c3f5d55b1ba312f5517f04059592a40d
unknown <khchu1220@naver.com>
2023-04-03 17:20:15 +0900

    Study : 유니온 타입의 장점

--- study/operator.ts
+++ study/operator.ts
@@ -6,8 +6,18 @@ logMessage1("hi");
 // logMessage(100) error
 // logMessage(false);
 
+let chu: string | number | boolean;
 const logMessage2 = (value: string | number) => {
-  console.log(value);
+  // 타입가드 : 특정 타입으로 타입의 범위를 좁혀나가는 필터링 하는 과정
+  if (typeof value === "number") {
+    value.toLocaleString();
+  }
+
+  if (typeof value === "string") {
+    value.toString();
+  }
+
+  throw new TypeError("value must be string or number");
 };
 
 logMessage2("123");
