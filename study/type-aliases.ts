c8879f3f02c58e830c5cf711b0825017186c534e
unknown <khchu1220@naver.com>
2023-04-03 14:34:33 +0900

    Study : 타입스크립트 별칭 사용

---
+++ study/type-aliases.ts
@@ -0,0 +1,21 @@
+interface Person1 {
+  name: string;
+  age: number;
+}
+
+type Person2 = {
+  name: string;
+  age: number;
+};
+
+let hoon: Person2 = {
+  name: "hoon",
+  age: 28,
+};
+
+type MyString = string;
+
+let str: MyString = "hello";
+
+type Todo = { id: string; title: string; done: boolean };
+function getTodo(todo: Todo) {}
