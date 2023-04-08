fc9955c7aaee2a742d78b3b17457a3b05feb1434
chuhoon <khchu1220@naver.com>
2023-04-08 15:56:20 +0900

    Study : 함수의 인자를 정의하는 인터페이스

--- study/interface-basic.ts
+++ study/interface-basic.ts
@@ -3,7 +3,19 @@ interface User {
   age: number;
 }
 
+// 변수 인터페이스
 let person: User = {
   name: 'chu',
   age: 12,
 };
+
+const getUser = (user: User) => {
+  console.log(user);
+};
+
+const user1 = {
+  name: 'chu',
+  age: 12,
+};
+
+getUser(user1);
