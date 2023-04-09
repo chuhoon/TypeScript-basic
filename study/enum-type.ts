da7cef77b5a86bdd82c6e9f100da42ad8d214ede
chuhoon <khchu1220@naver.com>
2023-04-09 20:09:51 +0900

    Study : 문자형 이넘

--- study/enum-type.ts
+++ study/enum-type.ts
@@ -5,3 +5,11 @@ enum Shoes {
 
 let myShoes = Shoes.Nike;
 console.log(myShoes); // 0 별도의 값을 지정하지 않으면 숫자로 지정함 adidas는 1
+
+enum Shirt {
+  Nike = '나이키',
+  Adidas = '아디다스',
+}
+
+let myShirts = Shirt.Adidas;
+console.log(myShirts);
