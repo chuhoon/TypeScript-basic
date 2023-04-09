83f03ad18b60335879cfdb699e1e7df9c9901204
chuhoon <khchu1220@naver.com>
2023-04-09 20:13:21 +0900

    Study : answer의 활용 사례

--- study/enum-type.ts
+++ study/enum-type.ts
@@ -13,3 +13,18 @@ enum Shirt {
 
 let myShirts = Shirt.Adidas;
 console.log(myShirts);
+
+enum Answer {
+  Yes = 'Y',
+  No = 'N',
+}
+
+const askQuestion = (answer: Answer) => {
+  if (answer === Answer.Yes) console.log('정답');
+  if (answer === Answer.No) console.log('오답');
+};
+askQuestion(Answer.Yes);
+askQuestion(Answer.No);
+// askQuestion('예스');
+// askQuestion('No');
+// askQuestion('Yes');
