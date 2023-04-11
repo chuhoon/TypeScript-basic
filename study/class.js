108bc4b816b339e1edc298b0cc86fd715cb6e01f
chuhoon <khchu1220@naver.com>
2023-04-11 21:49:46 +0900

    Study : 간단한 타입스크립트 클래스 문법

--- study/class.js
+++ study/class.js
@@ -1,13 +1,13 @@
-class Person {
-  // 클래스 로직
-  constructor(name, age) {
-    console.log('생성 되었습니다.');
-    this.name = name;
-    this.age = age;
-  }
-}
+// class Person {
+//   // 클래스 로직
+//   constructor(name, age) {
+//     console.log('생성 되었습니다.');
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
 
-let hoon = new Person('경훈', 11);
+// let hoon = new Person('경훈', 11);
 console.log(hoon); // Person 객체가 찍힘
 
 function Person2(name, age) {
---
+++ study/class.ts
@@ -0,0 +1,12 @@
+class Person {
+  // private name: string;
+  // public name: string;
+  // readonly name: string;
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
