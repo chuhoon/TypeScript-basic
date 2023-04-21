43ecb96edce6adbe320eeb90ab7f2902ca964fa1
unknown <khchu1220@naver.com>
2023-04-21 11:38:59 +0900

    Study : 타입 함수 중복 선언 단점

--- study/generic.ts
+++ study/generic.ts
@@ -7,9 +7,25 @@
 // logText("하이");
 // logText(true);
 
-function logText<T>(text: T): T {
+// function logText<T>(text: T): T {
+//   console.log(text);
+//   return text;
+// }
+
+// logText<string>("1");
+
+function logText(text: string) {
   console.log(text);
+  //   text.split("").reverse().join();
   return text;
 }
 
-logText<string>("1");
+function logNumber(num: number) {
+  console.log(num);
+  return num;
+}
+
+logText("1");
+logText(true);
+logNumber(10);
+logText(10);
