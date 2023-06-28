dcf3028c3f2839a9c87469c453856e66225b481d
chuhoon <khchu1220@naver.com>
2023-06-28 15:55:32 +0900

    Study : 타입호환 함수

--- study/type-compatibilty.ts
+++ study/type-compatibilty.ts
@@ -17,3 +17,36 @@ developer = person;
 */
 
 // developer = person;
+
+// 함수
+let add = function (a: number) {
+  // ...
+};
+
+let sum = function (a: number, b: number) {
+  // return a + b;
+};
+
+sum = add;
+// 타입이 더 크기 때문에 add에 sum이 타입호환이 되지 않음
+// add = sum;
+
+// 제네릭
+interface Empty<T> {
+  // ...
+}
+
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+
+empty1 = empty2;
+empty2 = empty1;
+
+interface NotEmpty<T> {
+  data: T;
+}
+
+var notEmpty1: NotEmpty<string>;
+var notEmpty2: NotEmpty<number>;
+notEmpty1 = notEmpty2;
+notEmpty2 = notEmpty1;
